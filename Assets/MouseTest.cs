8528a95500fa31c54c3816561690ec5362cd94d7
AdenHarvey <adenharvey@gmail.com>
2017-03-06 14:31:33 -0800

    Added MouseTest & PlayerMovement code.

---
+++ Assets/MouseTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTest : MonoBehaviour {
+	[SerializeField] GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 v = Input.mousePosition;
+		//v.z = 8.2f;
+
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			//Debug.Log(Input.GetAxis("Mouse X"));
+			if (Physics.Raycast (ray, out hit)) {
+				Debug.Log (hit.point);
+				player.transform.position (hit.point);
+			}
+			//Debug.Log(Camera.main.ScreenToWorldPoint(v));
+		}
+	}
+}
