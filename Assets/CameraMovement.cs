7f1bfcda72e80280587a83872868194e8ed6ede2
AdenHarvey <adenharvey@gmail.com>
2017-03-06 17:14:26 -0800

    Expanded on Camera Controls.

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
 	//[RequireComponent (Camera)]
+	[SerializeField] float cameraSpeed = 0.3f;
 
 	// Use this for initialization
 	void Start () {
@@ -16,11 +17,19 @@ public class CameraMovement : MonoBehaviour {
 		float y = Input.mousePosition.y;
 		float x = Input.mousePosition.x;
 		if (y >= Screen.height - 15.0f) {
-			this.transform.Translate (new Vector3(0.0f, 0.5f, 0.0f));
+			this.transform.Translate (new Vector3(0.0f, cameraSpeed, 0.0f));
 		}
 
 		if (y <= 15.0f) {
-			this.transform.Translate (new Vector3(0.0f, -0.5f, 0.0f));
+			this.transform.Translate (new Vector3(0.0f, -cameraSpeed, 0.0f));
+		}
+
+		if (x >= Screen.width - 15.0f) {
+			this.transform.Translate (new Vector3(cameraSpeed, 0.0f, 0.0f));
+		}
+
+		if (x <= 15.0f) {
+			this.transform.Translate (new Vector3(-cameraSpeed, 0.0f, 0.0f));
 		}
 	}
 }
