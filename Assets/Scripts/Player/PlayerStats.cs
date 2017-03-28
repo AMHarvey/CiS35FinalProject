7c1afc0b7a730a14d2e89e5951d7feacf4ebf97a
AdenHarvey <adenharvey@gmail.com>
2017-03-27 17:11:50 -0700

    Moved some scripts, started character stats.

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	private int[] _stats = new int[6];
+
+	private int _lowest = int.MaxValue;
+
+	// Use this for initialization
+	void Start () {
+		//int[] firstDice = d6(4);
+		//int totalDice = removeMin(firstDice);
+		stats();
+	}
+
+	private void stats() {
+		int[] dice = null;
+		int total = 0;
+		//Debug.Log(removeMin (d6(4)));
+		for (int i = 0; i < _stats.Length; i++) {
+			dice = d6 (4);
+			total = removeMin (dice);
+			_stats [i] = total;
+			Debug.Log (_stats [i]);
+		}
+	}
+
+
+	//TODO Put into diceroller class
+	private int[] d6(int diceNum) {
+		int[] d6 = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			d6 [i] = Random.Range (1, 6 + 1);
+		}
+		return d6;
+	}
+
+	private int removeMin (int[] diceArray) {
+		int temp = diceArray.Min ();
+		return diceArray.Sum () - temp;
+	}
+}
---
+++ Assets/Scripts/Player/StatEnums.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatEnums : MonoBehaviour {
+
+	enum Stats {
+		Str = 1,
+		Dex = 2,
+		Con = 3,
+		Int = 4,
+		Wis = 5,
+		Cha = 6
+	}
+
+	// Use this for initialization
+	void Start () {
+		//Stats = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
