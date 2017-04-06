f5fef27184562ff1327b20deabe68a3595f3ab67
AdenHarvey <adenharvey@gmail.com>
2017-04-05 17:26:00 -0700

    Made Dice Script.

---
+++ Assets/Scripts/Player/Dice.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+	public static int[] d4(int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, 4 + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int[] d6(int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, 6 + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int[] d8(int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, 8 + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int[] d10(int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, 10 + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int[] d12(int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, 12 + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int[] d20(int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, 20 + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int[] d100(int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, 100 + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int[] dCustom(int diceMax, int diceNum) {
+		int[] diceRolls = new int[diceNum];
+		for (int i = 0; i < diceNum; i++) {
+			diceRolls [i] = Random.Range (1, diceMax + 1);
+		}
+		return diceRolls;
+	}
+
+	public static int removeMin (int[] diceArray) {
+		return diceArray.Sum () - diceArray.Min ();
+	}
+}
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,56 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour {
 
-	enum statEnum { Str, Dex, Con, Int, Wis, Cha };
+	private enum statEnum { Str, Dex, Con, Int, Wis, Cha };
 
-	private int[] _stats = new int[6];
+	public int[] _stats = new int[6];
 
-	private int _lowest = int.MaxValue;
-
-	// Use this for initialization
 	void Start () {
 		stats();
 	}
 
 	private void stats() {
 		//Testing
-		//Debug.Log("Dice Working: " + tester ());
+		Debug.Log("Dice Working: " + tester ());
 
 		for (int i = 0; i < _stats.Length; i++) {
-			_stats [i] = removeMin(d6(4));
+			_stats [i] = Dice.removeMin(Dice.d6(4));
 			Debug.Log ((statEnum)i + ": " + _stats [i]);
 		}
 	}
 
-
-	//TODO Put into diceroller class
-	private int[] d6(int diceNum) {
-		int[] d6 = new int[diceNum];
-		for (int i = 0; i < diceNum; i++) {
-			d6 [i] = Random.Range (1, 6 + 1);
-		}
-		return d6;
-	}
-
-	private int removeMin (int[] diceArray) {
-		int temp = diceArray.Min ();
-		return diceArray.Sum () - temp;
-	}
-
+	//Testing Purposes. Remove when done.
 	private bool tester() {
-		int backOut = 0;
-		int total = 0;
+		int backOut, total;
+		backOut = total = 0;
 		while (backOut != 10000) {
-			total = removeMin (d6 (4));
+			total = Dice.removeMin (Dice.d6 (4));
 			backOut++;
-			if (total == 3) {
-				return true;
-			}
-		}
-		return false;
+			if (total == 3) { return true; } 
+		} 
+		return false; 
 	}
 }
